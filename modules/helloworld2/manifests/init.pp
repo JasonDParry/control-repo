7bc7b6c8ccf485f7c2d6714c525c582a7bec2478
JasonDParry <jasondparry@outlook.com>
2018-09-01 11:29:43 +0800

    Update init.pp

--- modules/helloworld2/manifests/init.pp
+++ modules/helloworld2/manifests/init.pp
@@ -20,7 +20,7 @@ class helloworld2 {
  
 exec { 'set-timezone':
   command   => 'Set-TimeZone -id \'W. Australia Standard Time\'',
-  unless    => 'if ((get-TimeZone).id -ne \'W. Australia Standard Time\') {exit 0}',
+  unless    => 'if ((get-TimeZone).id -ne \'W. Australia Standard Time\') {exit 1}',
   provider  => powershell,
   logoutput => true,
 }
